f0d2e5a9c5e3dcffd6702eac1e0a0e973b7579cb
ctgk <r1135nj54w@gmail.com>
2022-04-24 13:22:55 +0900

    DELETE: remove unnecessary methods from `pygrad.Graph`

--- pygrad/_core/_graph.py
+++ pygrad/_core/_graph.py
@@ -1,4 +1,3 @@
-from collections import OrderedDict
 import typing as tp
 
 import numpy as np
@@ -16,10 +15,6 @@ class Graph(object):
         """Construct computational graph."""
         super().__init__()
         self._operations: tp.List[_DifferentiableOperator] = []
-        self._parent = OrderedDict()
-        self._children = {}
-        self._backward_counts = {}
-        self._terminal = None
 
     def __enter__(self) -> 'Graph':
         """Return new computation graph to construct.
@@ -42,52 +37,6 @@ class Graph(object):
     def __exit__(self, *args, **kwargs):
         """Exit from the graph under construction."""
         config._graph = None
-
-    def _add_array_op(self, array, op):
-        for arg in op._args:
-            if arg in self._children:
-                self._children[arg].append(op)
-            else:
-                self._children[arg] = [op]
-            self._backward_counts[arg] = 0
-        self._backward_counts[array] = 0
-        self._children[array] = []
-        self._parent[array] = op
-        array._graph = self
-
-    def forward(self):
-        """Run forward propagation through the computational graph."""
-        for k in self._backward_counts:
-            self._backward_counts[k] = 0
-        for arr, op in self._parent.items():
-            for arg in op._args:
-                arg._num_backwards = 0
-                arg._grad = None
-            arr._num_backwards = 0
-            arr._grad = None
-            arr._data = op._forward_numpy(*tuple(a._data for a in op._args))
-
-    def _backward(self, a: Tensor):
-        if a not in self._parent:
-            return
-        op = self._parent[a]
-        dargs = op.backward(a._grad)
-        for darg, arg in zip(dargs, op._args):
-            if darg is not None:
-                if arg._grad is None:
-                    arg._grad = darg
-                else:
-                    arg._grad += darg
-                self._backward_counts[arg] += 1
-                if self._backward_counts[arg] == len(self._children[arg]):
-                    self._backward(arg)
-                elif self._backward_counts[arg] > len(self._children[arg]):
-                    raise ValueError()
-
-    def backward(self):
-        """Run backpropagation through the computational graph."""
-        self._terminal._grad = np.ones_like(self._terminal._data)
-        self._backward(self._terminal)
 
     @_typecheck()
     def gradient(
