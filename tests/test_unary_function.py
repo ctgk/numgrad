b57baa8a16aea33869a19c7863a022a5ffad5e42
ctgk <r1135nj54w@gmail.com>
2022-04-30 08:42:20 +0900

    CHANGE: remove `gd.Variable()`

--- tests/test_unary_function.py
+++ tests/test_unary_function.py
@@ -8,90 +8,76 @@ from pygrad._utils._numerical_grad import _numerical_grad
 
 @pytest.mark.parametrize('function, x', [
     # signs
-    (np.positive, gd.Variable(-3)),
-    (lambda a: +a, gd.Variable(-3)),
-    (np.negative, gd.Variable(-3)),
-    (lambda a: -a, gd.Variable(-3)),
+    (np.positive, -3),
+    (lambda a: +a, -3),
+    (np.negative, -3),
+    (lambda a: -a, -3),
 
     # four basic operations
-    (lambda a: np.add(a, [[1, 2], [3, 4]]), gd.Variable([1, 2])),
-    (lambda a: a + [[1, 2], [3, 4]], gd.Variable([1, 2])),
-    (lambda a: np.subtract(a, [[1, 2], [3, 4]]), gd.Variable([1, 2])),
-    (lambda a: a - [[1, 2], [3, 4]], gd.Variable([1, 2])),
-    (lambda a: np.multiply(a, [[1, 2], [3, 4]]), gd.Variable([1, 2])),
-    (lambda a: a * [[1, 2], [3, 4]], gd.Variable([1, 2])),
-    (lambda a: np.divide(a, [[1, 2], [3, 4]]), gd.Variable([1, 2])),
-    (lambda a: a / [[1, 2], [3, 4]], gd.Variable([1, 2])),
+    (lambda a: np.add(a, [[1, 2], [3, 4]]), [1, 2]),
+    (lambda a: a + [[1, 2], [3, 4]], [1, 2]),
+    (lambda a: np.subtract(a, [[1, 2], [3, 4]]), [1, 2]),
+    (lambda a: a - [[1, 2], [3, 4]], [1, 2]),
+    (lambda a: np.multiply(a, [[1, 2], [3, 4]]), [1, 2]),
+    (lambda a: a * [[1, 2], [3, 4]], [1, 2]),
+    (lambda a: np.divide(a, [[1, 2], [3, 4]]), [1, 2]),
+    (lambda a: a / [[1, 2], [3, 4]], [1, 2]),
 
     # powers
-    (np.square, gd.Variable([2, -1])),
-    (np.sqrt, gd.Variable([3, 0.5])),
+    (np.square, [2, -1]),
+    (np.sqrt, [3, 0.5]),
 
     # trigonometric functions
-    (np.cos, gd.Variable(np.random.uniform(-10, 10, (3, 2)))),
-    (np.sin, gd.Variable(np.random.uniform(-10, 10, (2, 5)))),
-    (np.tan, gd.Variable(np.random.uniform(-10, 10, (4, 1)))),
+    (np.cos, np.random.uniform(-10, 10, (3, 2))),
+    (np.sin, np.random.uniform(-10, 10, (2, 5))),
+    (np.tan, np.random.uniform(-10, 10, (4, 1))),
 
     # hyperbolic functions
-    (np.cosh, gd.Variable(np.random.uniform(-10, 10, (3, 4)))),
-    (np.sinh, gd.Variable(np.random.uniform(-10, 10, (1, 5)))),
-    (np.tanh, gd.Variable(np.random.uniform(-10, 10, (4, 2)))),
+    (np.cosh, np.random.uniform(-10, 10, (3, 4))),
+    (np.sinh, np.random.uniform(-10, 10, (1, 5))),
+    (np.tanh, np.random.uniform(-10, 10, (4, 2))),
 
     # exp & log
-    (np.exp, gd.Variable([-1, -0.2, 0.5])),
-    (np.log, gd.Variable([1, 0.2, 0.5, 2])),
+    (np.exp, [-1, -0.2, 0.5]),
+    (np.log, [1, 0.2, 0.5, 2]),
 
-    (lambda a: sp.log_softmax(a), gd.Variable([0.5, 0, -0.5])),
+    (lambda a: sp.log_softmax(a), [0.5, 0, -0.5]),
     (
         lambda a: sp.log_softmax(a, axis=-1),
-        gd.Variable([[0.5, 0, -0.5], [0, 1, 2]]),
+        [[0.5, 0, -0.5], [0, 1, 2]],
     ),
-    (lambda a: sp.logsumexp(a), gd.Variable(-1)),
-    (lambda a: sp.logsumexp(a), gd.Variable([-1, 1])),
-    (lambda a: sp.logsumexp(a, axis=1), gd.Variable(np.random.rand(3, 2))),
+    (lambda a: sp.logsumexp(a), -1),
+    (lambda a: sp.logsumexp(a), [-1, 1]),
+    (lambda a: sp.logsumexp(a, axis=1), np.random.rand(3, 2)),
     (
         lambda a: sp.logsumexp(a, axis=(0, 2), keepdims=True),
-        gd.Variable(np.random.rand(4, 2, 3)),
+        np.random.rand(4, 2, 3),
     ),
-    (
-        lambda a: sp.log_softmax(a, axis=(0, 2)),
-        gd.Variable(np.random.rand(2, 3, 4)),
-    ),
+    (lambda a: sp.log_softmax(a, axis=(0, 2)), np.random.rand(2, 3, 4)),
 
     # gamma function
-    (sp.gamma, gd.Variable([1, 0.5, 3.3])),
+    (sp.gamma, [1, 0.5, 3.3]),
 
     # stats
-    (np.max, gd.Variable(9)),
-    (np.max, gd.Variable([1, 2])),
-    (lambda a: a.max(axis=1), gd.Variable(np.random.rand(2, 3) * 10)),
-    (
-        lambda a: a.max(axis=(0, 2), keepdims=True),
-        gd.Variable(np.random.rand(2, 4, 3) * 10),
-    ),
-    (np.min, gd.Variable(9)),
-    (np.min, gd.Variable([1, 2])),
-    (lambda a: a.min(axis=1), gd.Variable(np.random.rand(2, 3) * 10)),
-    (
-        lambda a: a.min(axis=(0, 2), keepdims=True),
-        gd.Variable(np.random.rand(2, 4, 3) * 10),
-    ),
-    (lambda a: np.mean(a), gd.Variable(-1)),
-    (lambda a: np.mean(a), gd.Variable([-1, 1])),
-    (lambda a: a.mean(axis=1), gd.Variable(np.random.rand(3, 2))),
-    (
-        lambda a: np.mean(a, axis=(0, 2), keepdims=True),
-        gd.Variable(np.random.rand(4, 2, 3)),
-    ),
-    (lambda a: np.sum(a), gd.Variable(-1)),
-    (lambda a: np.sum(a), gd.Variable([-1, 1])),
-    (lambda a: a.sum(axis=1), gd.Variable(np.random.rand(3, 2))),
-    (
-        lambda a: np.sum(a, axis=(0, 2), keepdims=True),
-        gd.Variable(np.random.rand(4, 2, 3)),
-    ),
+    (np.max, 9),
+    (np.max, [1, 2]),
+    (lambda a: a.max(axis=1), np.random.rand(2, 3) * 10),
+    (lambda a: a.max(axis=(0, 2), keepdims=True), np.random.rand(2, 4, 3)),
+    (np.min, 9),
+    (np.min, [1, 2]),
+    (lambda a: a.min(axis=1), np.random.rand(2, 3) * 10),
+    (lambda a: a.min(axis=(0, 2), keepdims=True), np.random.rand(2, 4, 3)),
+    (lambda a: np.mean(a), -1),
+    (lambda a: np.mean(a), [-1, 1]),
+    (lambda a: a.mean(axis=1), np.random.rand(3, 2)),
+    (lambda a: np.mean(a, (0, 2), keepdims=True), np.random.rand(4, 2, 3)),
+    (lambda a: np.sum(a), -1),
+    (lambda a: np.sum(a), [-1, 1]),
+    (lambda a: a.sum(axis=1), np.random.rand(3, 2)),
+    (lambda a: np.sum(a, (0, 2), keepdims=True), np.random.rand(4, 2, 3)),
 ])
 def test_gradient(function, x):
+    x = gd.Variable(x)
     with gd.Graph() as g:
         y = function(x)
     dx_actual = g.gradient(y, [x])[0]
